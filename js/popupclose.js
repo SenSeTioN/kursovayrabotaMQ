03114546256c8121dddaf0ab6b9898fc5213358b
SenSeTioN <112203542+SenSeTioN@users.noreply.github.com>
2023-01-04 13:48:33 +0300

    changes

---
+++ js/popupclose.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("popupclose").addEventListener("click", function() {
+        document.getElementById("popup").classList.toggle("open")
+    })
+})
